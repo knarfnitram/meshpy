aab587f87e39409f3d4117db1e8c69e4b391f07d
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2026-01-16 11:15:57 +0100

    Follow up to #542

--- tests/integration/test_integration_four_c_model_importer.py
+++ tests/integration/test_integration_four_c_model_importer.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-"""Integration tests for the cubit interface of BeamMe."""
+"""Integration tests for the 4C model importer of BeamMe."""
 
 import re
 
